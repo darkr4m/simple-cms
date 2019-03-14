45bdebbba891583b1ba6bf7bbf4105f47133bee6
darkr4m <blackr4m@gmail.com>
2019-03-13 23:26:51 -0400

    Create error partial. Add validation for presence of name in all models. Create helper method for error messsages.

--- app/models/section.rb
+++ app/models/section.rb
@@ -8,4 +8,5 @@ class Section < ApplicationRecord
   scope :sorted_asc, lambda { order(:position => :asc) }
   scope :newest_first, lambda { where(:created_at => :desc) }
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
+  validates_presence_of :name
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -7,4 +7,6 @@ class Subject < ApplicationRecord
   scope :sorted_asc, lambda { order(:position => :asc) }
   scope :newest_first, lambda { where(:created_at => :desc) }
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
+
+  validates_presence_of :name
 end
