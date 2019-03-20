043925ec806126958825a808889be19edf92eb9f
darkr4m <blackr4m@gmail.com>
2019-03-20 16:34:39 -0400

    Add CRUD functionality for AdminUsers. Restrict access to AdminUsers CRUD.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,9 +5,23 @@ class AdminUser < ApplicationRecord
   has_many :sections, :through => :section_edits
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  FORBIDDEN_USERNAMES = ['admin', 'root', 'administrator']
 
   validates :first_name,  :presence => true, :length => {:maximum => 25}
   validates :last_name,  :presence => true, :length => {:maximum => 25}
   validates :username, :presence => true, :length => {:within => 5..25}, :uniqueness => true
   validates :email, :presence => true, :length => {:maximum => 50}, :uniqueness => true, :format => EMAIL_REGEX, :confirmation => true
+
+  scope :sorted, lambda { order('last_name ASC', 'first_name ASC') }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+  def username_is_allowed
+    if FORBIDDEN_USERNAMES.include?(username)
+      errors.add(:username, "has been restricted from use. Please try something different.")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
   get 'admin', :to => 'access#menu'
   get 'access/menu'
   get 'access/login'
   post 'access/attempt-login'
   get 'access/logout'
+
+  resources :admin_users, :except => [:show] do
+    member do
+      get :delete
+    end
+  end
 
   resources :subjects do
     member do
