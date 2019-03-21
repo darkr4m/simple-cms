ad3673bb711c698b1f3831165cf09ef503efb40f
darkr4m <blackr4m@gmail.com>
2019-03-21 15:53:33 -0400

    Add user identity feature on menu page.

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -3,6 +3,7 @@ class AccessController < ApplicationController
   before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
   def menu
     #display text and links
+    @username = session[:username]
   end
 
   def login
@@ -18,6 +19,7 @@ class AccessController < ApplicationController
     end
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
       flash[:notice] = "You are now logged in."
       redirect_to(admin_path)
     else
@@ -28,6 +30,7 @@ class AccessController < ApplicationController
 
   def logout
     session[:user_id] = nil
+    session[:username] = nil
     flash[:notice] = "Logged out."
     redirect_to(access_login_path)
   end
